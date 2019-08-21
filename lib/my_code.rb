f56074167b1eda975ee24cd4d323f471e03f31e8
Mark DeNardo <mark.denardo@gmail.com>
2019-08-21 16:35:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,47 +1,47 @@
 #require "pry"
 #my own map-like methods
 
-def map_to_negativize(source_array)
-  # source_array.map {|i|i*-1}
-  i=0
-  new_array=[]
-  while i < source_array.length do
-    new_array.push(source_array[i]*-1)
-    #binding.pry
-  i+=1
-  end
-  return new_array
-end
+# def map_to_negativize(source_array)
+#   # source_array.map {|i|i*-1}
+#   i=0
+#   new_array=[]
+#   while i < source_array.length do
+#     new_array.push(source_array[i]*-1)
+#     #binding.pry
+#   i+=1
+#   end
+#   return new_array
+# end
 
-def map_to_no_change(source_array)
-  i=0
-  new_array=[]
-  while i < source_array.length do
-    new_array.push(source_array[i])
-  i+=1
-  end
-  return new_array
-end
+# def map_to_no_change(source_array)
+#   i=0
+#   new_array=[]
+#   while i < source_array.length do
+#     new_array.push(source_array[i])
+#   i+=1
+#   end
+#   return new_array
+# end
 
-def  map_to_double(source_array)
-  i=0
-  new_array=[]
-  while i < source_array.length do
-    new_array.push(source_array[i]*2)
-  i+=1
-  end
-  return new_array
-end
+# def  map_to_double(source_array)
+#   i=0
+#   new_array=[]
+#   while i < source_array.length do
+#     new_array.push(source_array[i]*2)
+#   i+=1
+#   end
+#   return new_array
+# end
 
-def map_to_square(source_array)
-  i=0
-  new_array=[]
-  while i < source_array.length do
-    new_array.push(source_array[i]**2)
-  i+=1
-  end
-  return new_array
-end
+# def map_to_square(source_array)
+#   i=0
+#   new_array=[]
+#   while i < source_array.length do
+#     new_array.push(source_array[i]**2)
+#   i+=1
+#   end
+#   return new_array
+# end
 
 #my own reduce-like methods
 
@@ -52,17 +52,17 @@ def reduce_to_total(source_array, starting_point=0)
   return total
 end
 
-def reduce_to_all_true(source_array)
-  i=0
-  while i<source_array.length
-    if source_array[i] == true
-      i+=1
-    elsif source_array[i] == false
-     return source_array
-    end
- end
-end
+# def reduce_to_all_true(source_array)
+#   i=0
+#   while i<source_array.length
+#     if source_array[i] == true
+#       i+=1
+#     elsif source_array[i] == false
+#     return source_array
+#     end
+# end
+# end
 
-def reduce_to_any_true
+# def reduce_to_any_true
   
-end
+# end
