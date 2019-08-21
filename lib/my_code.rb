c4e1c16e2b59f09cea3b2af71f5c38e27855b289
Mark DeNardo <mark.denardo@gmail.com>
2019-08-21 16:46:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,47 +1,47 @@
 #require "pry"
 #my own map-like methods
 
-# def map_to_negativize(source_array)
-#   # source_array.map {|i|i*-1}
-#   i=0
-#   new_array=[]
-#   while i < source_array.length do
-#     new_array.push(source_array[i]*-1)
-#     #binding.pry
-#   i+=1
-#   end
-#   return new_array
-# end
+def map_to_negativize(source_array)
+  # source_array.map {|i|i*-1}
+  i=0
+  new_array=[]
+  while i < source_array.length do
+    new_array.push(source_array[i]*-1)
+    #binding.pry
+  i+=1
+  end
+  return new_array
+end
 
-# def map_to_no_change(source_array)
-#   i=0
-#   new_array=[]
-#   while i < source_array.length do
-#     new_array.push(source_array[i])
-#   i+=1
-#   end
-#   return new_array
-# end
+def map_to_no_change(source_array)
+  i=0
+  new_array=[]
+  while i < source_array.length do
+    new_array.push(source_array[i])
+  i+=1
+  end
+  return new_array
+end
 
-# def  map_to_double(source_array)
-#   i=0
-#   new_array=[]
-#   while i < source_array.length do
-#     new_array.push(source_array[i]*2)
-#   i+=1
-#   end
-#   return new_array
-# end
+def  map_to_double(source_array)
+  i=0
+  new_array=[]
+  while i < source_array.length do
+    new_array.push(source_array[i]*2)
+  i+=1
+  end
+  return new_array
+end
 
-# def map_to_square(source_array)
-#   i=0
-#   new_array=[]
-#   while i < source_array.length do
-#     new_array.push(source_array[i]**2)
-#   i+=1
-#   end
-#   return new_array
-# end
+def map_to_square(source_array)
+  i=0
+  new_array=[]
+  while i < source_array.length do
+    new_array.push(source_array[i]**2)
+  i+=1
+  end
+  return new_array
+end
 
 #my own reduce-like methods
 
