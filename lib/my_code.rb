192989f368e5afc2a2bacbceeba24c64f55b4631
Mark DeNardo <mark.denardo@gmail.com>
2019-08-21 16:38:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,16 +55,16 @@ def reduce_to_total(source_array, starting_point=0)
   return total
 end
 
-# def reduce_to_all_true(source_array)
-#   i=0
-#   while i<source_array.length
-#     if source_array[i] == true
-#       i+=1
-#     elsif source_array[i] == false
-#     return source_array
-#     end
-# end
-# end
+def reduce_to_all_true(source_array)
+  i=0
+  while i<source_array.length
+    if source_array[i] == true
+      i+=1
+    elsif source_array[i] == false
+    return source_array
+    end
+end
+end
 
 # def reduce_to_any_true
   
