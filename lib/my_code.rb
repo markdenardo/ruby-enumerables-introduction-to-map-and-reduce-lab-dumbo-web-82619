242c4325886e4b2f8aedd09f5d47d84671482330
Mark DeNardo <mark.denardo@gmail.com>
2019-08-21 16:41:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,10 +58,10 @@ end
 def reduce_to_all_true(source_array)
   i=0
     while i<source_array.length do
- source_array[i].true?
+ source_array[i].
        i+=1
     end
-  return 
+  
 end
 
 # def reduce_to_any_true
